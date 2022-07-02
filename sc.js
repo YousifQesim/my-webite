11936ec1c25977c4e405113bb207f35ac129d44f
yusifqasim <yusifqasim22@gmail.com>
2022-07-02 16:02:46 +0300

    burger style changed

--- sc.js
+++ sc.js
@@ -7,16 +7,21 @@ var typed = new Typed('.auto', {
 let toggle=document.getElementById("toggle");
 let for_res=document.getElementById("for_res");
 let navbar=document.getElementById("navbar");
+let bar1=document.getElementById("bar1");
+let bar2=document.getElementById("bar2");
+let bar3=document.getElementById("bar3");
 function press() {
   if (for_res.classList.contains("active")) {
    
     for_res.classList.remove("active");
     navbar.style.display="none";
-    
+    toggle.classList.remove("toggle-changer");
+   
    
 
   }
   else{
+    toggle.classList.add("toggle-changer");
     for_res.classList.add("active");
     for_res.style.width="50vw";
     navbar.style.display="block";
