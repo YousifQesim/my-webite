ff1877ccf13e841f784e2f4d70ec75b55bb7b1b8
yusifqasim <yusifqasim22@gmail.com>
2022-07-02 14:51:30 +0300

    navbar responsived

--- sc.js
+++ sc.js
@@ -4,3 +4,22 @@ var typed = new Typed('.auto', {
     backSpeed:50,
     loop:true
   });
+let toggle=document.getElementById("toggle");
+let for_res=document.getElementById("for_res");
+let navbar=document.getElementById("navbar");
+function press() {
+  if (for_res.classList.contains("active")) {
+   
+    for_res.classList.remove("active");
+    navbar.style.display="none";
+    
+   
+
+  }
+  else{
+    for_res.classList.add("active");
+    for_res.style.width="30vw";
+    navbar.style.display="block";
+  }
+  
+}
